5cbf0b7edb7c1976357f5135c74de81b872dd22f
walid.sadou <walid.sadou@ibm.com>
2018-06-18 13:00:09 +0200

    travis test

--- test.js
+++ test.js
@@ -7,12 +7,16 @@ var testdb = nano.use('testdb');
 describe('couchdbtest', function() {
    it('should find database', function(done) {
        nano.db.get('testdb', function (err, body) {
-           should.equal(body, 'testdb');
+           should.equal(body.db_name, 'testdb');
+           done;
        })
    });
    it('should find good doc', function() {
-       testdb.get('myid', function(err, body) {
+       testdb.get('{ ok: true,\n' +
+           '  id: \'myid\',\n' +
+           '  rev: \'1-6e4cb465d49c0368ac3946506d26335d\' }', function(err, body) {
            should.equal(body, 'myid');
+           done;
        });
    })
 });
